83ef3ab8e13b65d439c04c507966f9b0138ec3ad
Nguyen Huy Binh <nguyenhuybinh270709@gmail.com>
2026-05-24 14:40:34 +0700

    feat(news): implement news detail page UI and fetch data from Wordpress API

--- src/app/news/[slug]/page.tsx
+++ src/app/news/[slug]/page.tsx
@@ -1,4 +1,11 @@
-import { PostViewTracker } from "@/app/news/[slug]/_components";
+import {
+  Author,
+  Content,
+  Hero,
+  PostViewTracker,
+  Sidebar,
+} from "@/app/news/[slug]/_components";
+import { News } from "@/types/wordpress";
 
 export default async function NewsDetails({
   params,
@@ -8,6 +15,9 @@ export default async function NewsDetails({
   const { slug } = await params;
 
   const REST_URL = process.env.NEXT_PUBLIC_WP_REST_URL;
+  if (!REST_URL) {
+    throw new Error("NEXT_PUBLIC_WP_REST_URL is missing in .env");
+  }
 
   const res = await fetch(`${REST_URL}/news?slug=${slug}&_embed`, {
     cache: "no-store",
@@ -15,18 +25,38 @@ export default async function NewsDetails({
 
   const data = await res.json();
 
-  const post = data[0];
+  const post: News = data[0];
 
   if (!post) {
     return <div>Không tìm thấy bài viết</div>;
   }
+
+  const relatedRes = await fetch(`${REST_URL}/news?_embed&per_page=4`, {
+    cache: "no-store",
+  });
+
+  const relatedData: News[] = await relatedRes.json();
+
+  const relatedPosts = relatedData.filter(
+    (item) => item.id !== post.id && item.acf.topic === post.acf.topic,
+  );
 
   return (
     <>
       <PostViewTracker postId={post.id} />
 
       <main>
-        <h1>{post.title.rendered}</h1>
+        <Hero post={post} />
+
+        <Author post={post} />
+        <div className="grid grid-cols-1 lg:grid-cols-9 gap-4 mt-1 mb-12 px-6 lg:px-12">
+          <div className="lg:col-span-6">
+            <Content post={post} />
+          </div>
+          <aside className="lg:col-span-3">
+            <Sidebar posts={relatedPosts} />
+          </aside>
+        </div>
       </main>
     </>
   );
